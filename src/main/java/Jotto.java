14ddb4af78287ae5ad55e9fb396d0a11abf4457e
julian-men <ianmenjones@gmail.com>
2026-02-12 13:51:30 -0800

    updated skeleton for future use, added student header

--- src/main/java/Jotto.java
+++ src/main/java/Jotto.java
@@ -1,7 +1,89 @@
+import java.util.ArrayList;
+
 /**
  * @author feng3302
  * @version 0.1.0
  * @Since 1/29/26
  **/
+//Author: Julian Mendoza
+//Date: 1/29/26
+//Description: The program will read a list of 5 letter words, take a word at random,
+// and have the user attempt to guess with matching letters equating to points.
+// It will keep track of words guessed and words chosen by the computer
 public class Jotto {
+    private static final int WORD_SIZE = 5;
+    private static final boolean DEBUG = true;
+
+    private String currentWord;
+    private int score;
+    private final ArrayList<String> playGuesses = new ArrayList<>();
+    private final ArrayList<String> playWords = new ArrayList<>();
+    private String filename;
+    private final ArrayList<String> wordList = new ArrayList<>();
+
+    public Jotto(String filename)
+    {
+        this.filename = filename;
+    }
+
+    public boolean pickWord()
+    {
+        return false;
+    }
+
+    public String showWordList()
+    {
+        return "";
+    }
+
+    public ArrayList<String> showPlayerGuesses()
+    {
+        return playGuesses;
+    }
+
+    public void playerGuessScores(ArrayList<String> guesses)
+    {
+
+    }
+    public void setCurrentWord(String currentWord) {
+        this.currentWord = currentWord;
+    }
+
+    public ArrayList<String> readWords()
+    {
+        return wordList;
+    }
+
+    public void play()
+    {
+
+    }
+
+    public int guess()
+    {
+        return 0;
+    }
+
+    public String getCurrentWord() {
+        return currentWord;
+    }
+
+    public ArrayList<String> getPlayWords() {
+        return playWords;
+    }
+
+    public int getLetterCount(String wordGuess)
+    {
+        return 0;
+    }
+
+    public boolean addPlayerGuess(String wordGuess)
+    {
+        return false;
+    }
+
+    public void updateWordList()
+    {
+
+    }
 }
